6e2ba7a603d06806b4b76451f5777c7c650bbe8d
AGUSTIN <agustinhillar@hotmail.com>
2025-01-16 18:20:00 -0300

    Agregado de README.md y modificaciones finales al app.js

---
+++ README.md
@@ -0,0 +1,16 @@
+
+APERTURA DE LA APLICACIÓN EN EL BROWSER
+
+Para ejecutar la aplicación, primero instale Live Server. 
+Luego, posicionarse en el archivo llamado index.hmtl. 
+Por último, presione el botón de GoLive que esta abajo a la derecha.
+
+Otra forma es:
+Hacer click derecho sobre index.html, presionar Reveal In File Explorer. 
+
+DETALLES DE FUNCIONALIDADES 
+
+Es una aplicación básica, que permite ingresar los nombres de tus amigos de la siguiente manera:
+ - Ingresamos el nombre que queremos y se agrega a la lista que va apareciendo debajo.
+ - Cuando agregamos un nombre, se limpia el campo para ingresar los mismos.
+ - Una vez hayamos ingresado todos los nombres, podemos apretar en Sortear amigo y se realiza el mismo.
--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Inicializo la lista de nombres de amigos
+let nombres = [];
+console.log(nombres)
+// Recupero la lista de resultado del DOM
+let resultado = document.getElementById("resultado")
+
+function agregarAmigo() {
+    // Recupero el nombre ingresado por el usuario
+    let input = document.getElementById("amigo")
+    let nombre = input.value
+    // Verifico que no sea vacio
+    if (nombre == "") {
+        alert("Debe ingresar un nombre")
+    } else {
+    //Si no es vacío, lo agrego al arreglo de nombres, limpio la caja para ingresar nombres y actualizo la lista
+    nombres.push(nombre);
+    input.value = "";
+    actualizarAmigos();
+}
+}
+
+function actualizarAmigos() {
+    // Recupero la lista del documento
+    let listaAmigos = document.getElementById("listaAmigos");
+    // La limpio
+    listaAmigos.innerHTML = "";
+    // Recorro el arreglo de nombres y por cada nombre creo un elemento de lista que agrego a la lista ul del DOM
+    for (let i = 0; i < nombres.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = nombres[i];
+        listaAmigos.appendChild(li)
+    }
+}
+
+function sortearAmigo() {
+    // Genero un índice aleatorio
+    let indiceAleatorio = Math.floor(Math.random()*(nombres.length))
+    if (nombres.length == 0) {
+        alert("No hay amigos para sortear!")
+    } else {
+        // Si hay nombres en la lista, entonces el ganador es aquel cuyo índice coindice con el indice aleatorio generado
+        let nombreSeleccionado = nombres[indiceAleatorio]
+        // Cambio a través de innerHTML el valor de la lista de resultado del DOM
+        resultado.innerHTML = "El amigo seleccionado en el sorteo fue: " + nombreSeleccionado;        
+        } 
+    }
+
+
